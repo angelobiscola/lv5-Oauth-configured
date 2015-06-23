523abc6303b68d7eecc0f4852ebe3bb742782be4
Angelo Biscola <angelo-tec@hotmail.com>
2015-06-23 14:06:17 -0300

    first commit

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('home', 'HomeController@index');
+
+Route::controllers([
+    'auth' => 'Auth\AuthController',
+    'password' => 'Auth\PasswordController',
+]);
